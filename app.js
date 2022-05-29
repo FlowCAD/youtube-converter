cead68dde96d92ca3d04e5c6155ee8a3e5037354
Florian Cadoz <fcadoz@waycom.net>
2022-05-29 17:36:45 +0200

    Backend modified for output quality purpose

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const readline = require('readline');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+
 require('dotenv').config();
 
 const app = express();
@@ -13,36 +16,49 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.render('index'));
 
-app.post('/convert-mp3', async (req, res) => {
-  const videoID = req.body.videoID;
-  if ([undefined, '', null].includes(videoID)) {
-    return res.render('index', {success: false, message: 'Please enter a video ID.'});
-  } else {
-    const url = `https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}`;
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Host': process.env.YOUTUBE_API_HOST,
-        'X-RapidAPI-Key': process.env.YOUTUBE_API_KEY
-      }
-    };
-    const fetchAPI = await fetch(url, options);
-    const fetchResponse = await fetchAPI.json();
+app.post('/download', async (req, res) => {
+  const videoURL = req.body.videoURL;
 
-    if (fetchResponse.status === 'ok') {
-      return res.render('index', {
-        success: true,
-        song_title: fetchResponse.title,
-        song_link: fetchResponse.link
-      });
-    } else {
-      return res.render('index', {
-        success: false,
-        message: fetchResponse.msg
-      });
-    }
+  if ([undefined, '', null].includes(videoURL)) {
+    return res.render('index', {success: false, message: `Merci d'entrer une URL Youtube valide.`});
+  } else {
+    const info = await ytdl.getInfo(videoURL)
+    if (!info.videoDetails) return;
+    const song = {
+      title: info.videoDetails.media.song || info.videoDetails.title || 'music',
+      artist: info.videoDetails.media.artist || null,
+      album: info.videoDetails.media.album || null
+    };
+
+    const stream = ytdl(videoURL, { quality: 'highestaudio' });
+    //set response headers
+    res.setHeader('Content-disposition', `attachment; filename=${song.title}.mp3`);
+    res.setHeader('Content-type', 'audio/mpeg');
+
+    //set stream for conversion
+    var proc = new ffmpeg({source: stream});
+
+    let start = Date.now();
+    proc
+      .withAudioCodec('libmp3lame')
+      .audioBitrate(320)
+      .format('mp3')
+      .outputOptions(
+        '-metadata', `title=${song.title}`,
+        '-metadata', `artist=${song.artist}`,
+        '-metadata', `album=${song.album}`,
+        '-metadata', `album_artist=${song.artist}`
+      )
+      .output(res)
+      .run();
+
+    proc.on('progress', p => {
+      readline.cursorTo(process.stdout, 0);
+      process.stdout.write(`${p.targetSize}kb téléchargés`);
+    })
+
+    proc.on('end', () => {console.log(`\nConversion effectuée en ${(Date.now() - start) / 1000}s`)});
   }
 });
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
