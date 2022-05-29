780fa69a03b51ab91d2e4076a1a6e085836ad71a
Florian Cadoz <fcadoz@waycom.net>
2022-05-29 17:37:00 +0200

    Clean code

--- app.js
+++ app.js
@@ -19,46 +19,42 @@ app.get('/', (req, res) => res.render('index'));
 app.post('/download', async (req, res) => {
   const videoURL = req.body.videoURL;
 
-  if ([undefined, '', null].includes(videoURL)) {
-    return res.render('index', {success: false, message: `Merci d'entrer une URL Youtube valide.`});
-  } else {
-    const info = await ytdl.getInfo(videoURL)
-    if (!info.videoDetails) return;
-    const song = {
-      title: info.videoDetails.media.song || info.videoDetails.title || 'music',
-      artist: info.videoDetails.media.artist || null,
-      album: info.videoDetails.media.album || null
-    };
+  const info = await ytdl.getInfo(videoURL)
+  if (!info.videoDetails) return;
+  const song = {
+    title: info.videoDetails.media.song || info.videoDetails.title || 'music',
+    artist: info.videoDetails.media.artist || null,
+    album: info.videoDetails.media.album || null
+  };
 
-    const stream = ytdl(videoURL, { quality: 'highestaudio' });
-    //set response headers
-    res.setHeader('Content-disposition', `attachment; filename=${song.title}.mp3`);
-    res.setHeader('Content-type', 'audio/mpeg');
+  const stream = ytdl(videoURL, { quality: 'highestaudio' });
+  //set response headers
+  res.setHeader('Content-disposition', `attachment; filename=${song.title}.mp3`);
+  res.setHeader('Content-type', 'audio/mpeg');
 
-    //set stream for conversion
-    var proc = new ffmpeg({source: stream});
+  //set stream for conversion
+  var proc = new ffmpeg({source: stream});
 
-    let start = Date.now();
-    proc
-      .withAudioCodec('libmp3lame')
-      .audioBitrate(320)
-      .format('mp3')
-      .outputOptions(
-        '-metadata', `title=${song.title}`,
-        '-metadata', `artist=${song.artist}`,
-        '-metadata', `album=${song.album}`,
-        '-metadata', `album_artist=${song.artist}`
-      )
-      .output(res)
-      .run();
+  let start = Date.now();
+  proc
+    .withAudioCodec('libmp3lame')
+    .audioBitrate(320)
+    .format('mp3')
+    .outputOptions(
+      '-metadata', `title=${song.title}`,
+      '-metadata', `artist=${song.artist}`,
+      '-metadata', `album=${song.album}`,
+      '-metadata', `album_artist=${song.artist}`
+    )
+    .output(res)
+    .run();
 
-    proc.on('progress', p => {
-      readline.cursorTo(process.stdout, 0);
-      process.stdout.write(`${p.targetSize}kb téléchargés`);
-    })
+  proc.on('progress', p => {
+    readline.cursorTo(process.stdout, 0);
+    process.stdout.write(`${p.targetSize}kb téléchargés`);
+  })
 
-    proc.on('end', () => {console.log(`\nConversion effectuée en ${(Date.now() - start) / 1000}s`)});
-  }
+  proc.on('end', () => {console.log(`\nConversion effectuée en ${(Date.now() - start) / 1000}s`)});
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
